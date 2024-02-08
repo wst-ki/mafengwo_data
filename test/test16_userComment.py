e8d0904252b2a0802880166a561204e2f0b8ba4f
Rur1sama <1401564074@qq.com>
2024-02-09 03:16:13 +0800

    class_01_mongodb.py完善功能 添加了初始化MongoDB时对数据库和集合存在性判断 添加了数据库各种方法的注释 test16添加了入库测试，

--- test/test16_userComment.py
+++ test/test16_userComment.py
@@ -2,5 +2,8 @@
 # 开发时间19:14,2024/2/5
 # name:test16_userComment
 # 测试function09
+from functions.class_01_mongodb import MongoDB
 from functions.function_09_getUserComment import  getUserComment
-getUserComment(76742257)
+my_mongo = MongoDB(dataset='地途', collection='user_comments')  # 新建或连接一个数据库实例
+user_info = getUserComment(76742257)
+my_mongo.insert_one(user_info)
