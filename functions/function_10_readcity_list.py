d803347c8fcaad6c4da8ecaa472f8e77c8eebc32
Rur1sama <1401564074@qq.com>
2024-04-05 10:50:31 +0800

    更新功能：批量爬取所有城市前300个poi数据 - 添加函数function10，用于批量从数据库读取城市id - 更新main中getCitiesData.py文件，启动爬取所有城市前300个poi数据的功能，并小修小改了一些地方

---
+++ functions/function_10_readcity_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project ：mafengwo_data 
+@File    ：function_10_readcity_list.py
+@IDE     ：PyCharm 
+@Author  ：lee_yukhang
+@Date    ：2024/4/5 10:25 
+"""
+from functions.class_01_mongodb import MongoDB
+
+
+def readcity_list(mongo_instance):
+    '''
+    本函数是读取cities_list文档（城市列表数据）中每一个城市的对应id，传入一个数据库实例，返回一个城市id列表
+    :param mongo_instance: 存有城市poi数据的数据库实例
+    :return: 城市id列表
+    '''
+    # city_mongo = MongoDB(dataset='地途', collection='cities_data')  # 连接城市数据库实例
+    city_list = []
+    # 查询所有文档，并且仅返回 "id" 字段
+    result = mongo_instance.find({}, {"id": 1, "_id": 0})
+    # 遍历查询结果，并将 id 添加到列表中
+    for document in result:
+        city_list.append(document["id"])
+        # 返回包含所有 id 的列表
+    print(city_list)
+    return city_list
--- main/getCitiesData.py
+++ main/getCitiesData.py
@@ -17,14 +17,16 @@
 from functions.class_01_mongodb import MongoDB
 from functions.function_04_getCitiesData_MongoDB import getCitiesData_DB
 from functions.function_08_getPOIComment_MongoDB import get_POIcomment_DB
+from functions.function_10_readcity_list import readcity_list
+
 # 爬取城市信息：使用func04-数据库版
 city_mongo = MongoDB(dataset='地途', collection='cities_data')  # 新建或连接一个数据库实例
-getCitiesData_DB(city_mongo)  # 若集合里面有数据，就会不爬了，重复爬取请删除数据
+getCitiesData_DB(city_mongo)  # 用于爬取所有城市列表到cities_data里面，若集合里面有数据，就会不爬了，重复爬取请删除数据
 
 # 爬取目的地信息(城市的POI信息)： 使用func05-getPOIID
 
 # todo 将上述爬取的对应城市信息用于爬取其前300个POI
-cityID_list = [10088]
+cityID_list = readcity_list(city_mongo)  # cityID_list 是一个列表，存储着所有城市的id
 for cityID in cityID_list:
     cities_POIs_mongo = MongoDB(dataset='地途', collection=f'{cityID}_POIs')  # 新建或连接一个数据库实例
     get_POIcomment_DB(cityID, cities_POIs_mongo)
